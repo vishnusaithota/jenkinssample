0b5db4c4f3a20d7ae69d7303b61ccefc452bb30d
vt <vishst98@gmail.com>
2022-12-19 22:30:14 -0500

    first push

---
+++ src/main/java/com/cicd/springBootApplicationWithJenkins/SpringBootApplicationWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.springBootApplicationWithJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationWithJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/springBootApplicationWithJenkins/SpringBootApplicationWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.springBootApplicationWithJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
